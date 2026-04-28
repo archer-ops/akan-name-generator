3ba43e9c7168327a7ffe236324edf4cfc0efb4b5
yvonne <yvonneandanje021@gmail.com>
2026-04-28 10:02:42 +0300

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+// Ghanan names(akan)
+// the end result //Akan names mapped to the formula
+//the results should be:1=monday, 2=tuesday, 3=wednesday, 4=thursday, 5=friday, 6=saturday, 7=sunday
+
+const maleNames = ["kwadwo", "kwabena", "kwaku", "yaw", "kofi", "kwame", "kwasi"]
+const femaleNames = ["adowa", "abenaa", "akua", "yaa", "afua", "ama", "akosua"]
+
+co
